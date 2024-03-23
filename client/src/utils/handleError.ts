54ee4c8349b9f01429609382d95441e27ccf5f58
rajesh <rajeshsharmax2c@gmail.com>
2024-03-23 21:58:42 +0530

    final

--- client/src/utils/handleError.ts
+++ client/src/utils/handleError.ts
@@ -1,6 +1,7 @@
 import { toast } from "sonner";
 
 export const handleError = (error: unknown) => {
+  //unknown error found
   console.log(error);
   //@ts=expect-error unknown
   console.log(error.data.message);
