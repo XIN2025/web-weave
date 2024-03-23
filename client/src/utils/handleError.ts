b83ab90b6d2286bab716e43efcac5600a3c83e22
rajesh <rajeshsharmax2c@gmail.com>
2024-03-23 15:09:23 +0530

    inital commit

---
+++ client/src/utils/handleError.ts
@@ -0,0 +1,7 @@
+import { toast } from "sonner";
+
+export const handleError = (error: any) => {
+  console.log(error);
+  console.log(error.data.message);
+  toast("Error: " + error.data.message);
+};
